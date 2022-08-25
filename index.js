a27dc085b881e5401928005154b91d9f0c1733b3
michal <michalwagh@gmail.com>
2022-08-25 09:23:36 +0100

    added js amd style files

---
+++ index.js
@@ -0,0 +1,21 @@
+const beginerLevel = document.querySelector(".welcome__level1");
+const gameSection
+
+const welcome = document.querySelector(".welcome");
+
+
+
+const cleanWindow = () => {
+
+    welcome.innerHTML = " ";
+
+}
+
+
+const runGame = () => {
+
+    cleanWindow()
+}
+
+
+beginerLevel.addEventListener("click", runGame)
