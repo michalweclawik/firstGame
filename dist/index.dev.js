8e851fc1f79fa0e6524a1af2fffdc89b91640c44
michal <michalwagh@gmail.com>
2022-08-29 16:58:44 +0100

    gameover page

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,33 +1,52 @@
 "use strict";
 
 // welcome section
-var buttons = document.querySelectorAll('.welcome__level'); // const beginerLevel = document.querySelector(".welcome__level1");
-// const mediumLevel = document.querySelector(".welcome__level2");
-// const expertLevel = document.querySelector(".welcome__level3");
-// game section
-
+var buttons = document.querySelectorAll('.welcome__level');
 var holes = document.querySelectorAll(".gameSection__playGroundSection");
 var playWindow = document.querySelector(".gameSection__playGround");
 var welcome = document.querySelector(".welcome");
 var cursor = document.querySelector(".malet");
+var gameTimer = document.querySelector("#timer");
 var gameButtons = document.querySelectorAll('.buttons');
 var scoreDisplay = document.querySelector('#gameSection__result');
 var startButton = document.querySelector('#gameSection__startButton');
 var resetButton = document.querySelector('#gameSection__resetButton');
 var gameSection = document.querySelector('.gameSection');
+var gameOver = document.querySelector(".gameoverSection");
+var retryButton = document.querySelector('.gameoverSection__retry-button');
+var gameoverScore = document.querySelector('.gameoverSection__yourScore');
 var onOFF = false;
 var score = 0;
 var gameSpeed = 0;
+var gameTime = null;
+var interval;
+var currentgameTime = null;
+
+resetGame = function resetGame() {
+  cleanWindow();
+  welcome.style.display = '';
+  gameSection.style.display = 'none';
+  clearInterval(interval);
+  gameOver.style.visibility = 'hidden';
+};
+
+retryButton.addEventListener('click', resetGame);
 
 var setLevel = function setLevel(event) {
   console.log(event.target.innerHTML);
 
   if (event.target.innerHTML === 'Beginer') {
     gameSpeed = 1500;
+    gameTime = 60;
+    gameTimer.textContent = gameTime;
   } else if (event.target.innerHTML === 'Medium') {
-    gameSpeed = 100;
+    gameSpeed = 1000;
+    gameTime = 50;
+    gameTimer.textContent = gameTime;
   } else if (event.target.innerHTML === 'Expert') {
     gameSpeed = 500;
+    gameTime = 40;
+    gameTimer.textContent = gameTime;
   }
 
   switchToSecondPage();
@@ -53,6 +72,7 @@ var switchToSecondPage = function switchToSecondPage() {
 
 var cleanWindow = function cleanWindow() {
   scoreDisplay.innerHTML = '';
+  score = 0;
   onOFF = null;
 };
 
@@ -65,7 +85,6 @@ var moleStart = function moleStart() {
     moleImage.classList.add("mole");
     moleImage.src = './molebody.png';
     moleImage.addEventListener('click', function () {
-      console.log("pressed");
       score += 5;
       scoreDisplay.innerHTML = score;
       moleImage.src = './molebodyAfter.png';
@@ -105,17 +124,36 @@ var startGame = function startGame() {
 var gameButtonsAction = function gameButtonsAction(event) {
   if (event.target.innerHTML === 'START') {
     startGame();
+    gameTimer.textContent = gameTime;
+    interval = setInterval(countDownTimer, 1000);
+    countDownTimer();
   }
 
   if (event.target.innerHTML === 'RESET') {
     cleanWindow();
+    clearInterval(interval);
+    interval = '';
+    gameTimer.textContent = gameTime;
+    currentgameTime = null;
   }
 
   if (event.target.innerHTML === 'GO BACK') {
     welcome.style.display = '';
-    gameSection.style.display = 'none'; // startButton.style.display = 'unset';
-    // resetButton.style.display = 'unset';
-    // gameButtons.style.display = 'none';
+    gameSection.style.display = 'none';
+    clearInterval(interval);
+  }
+};
+
+var countDownTimer = function countDownTimer() {
+  currentgameTime = gameTimer.textContent;
+  currentgameTime--;
+  gameTimer.textContent = currentgameTime;
+
+  if (currentgameTime == 0) {
+    clearInterval(interval); // cleanWindow()
+
+    gameOver.style.visibility = 'visible';
+    gameoverScore.innerHTML += score;
   }
 };
 
--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 // welcome section
 const buttons = document.querySelectorAll('.welcome__level');
-// const beginerLevel = document.querySelector(".welcome__level1");
-// const mediumLevel = document.querySelector(".welcome__level2");
-// const expertLevel = document.querySelector(".welcome__level3");
-// game section
+
 
 const holes = document.querySelectorAll(".gameSection__playGroundSection")
 const playWindow = document.querySelector(".gameSection__playGround")
 const welcome = document.querySelector(".welcome");
 const cursor = document.querySelector(".malet");
-
+const gameTimer = document.querySelector("#timer");
 
 
 const gameButtons = document.querySelectorAll('.buttons')
@@ -19,18 +16,44 @@ const resetButton = document.querySelector('#gameSection__resetButton')
 const gameSection = document.querySelector('.gameSection')
 
 
+const gameOver = document.querySelector(".gameoverSection")
+const retryButton = document.querySelector('.gameoverSection__retry-button')
+const gameoverScore = document.querySelector('.gameoverSection__yourScore')
+
 let onOFF = false;
 let score = 0;
 let gameSpeed = 0;
+let gameTime = null;
+let interval
+let currentgameTime = null;
+
+
+resetGame = () => {
+    cleanWindow()
+    welcome.style.display = '';
+    gameSection.style.display = 'none';
+    clearInterval(interval);
+    gameOver.style.visibility = 'hidden';
+}
+
+
+retryButton.addEventListener('click', resetGame);
+
 
 const setLevel = (event) => {
     console.log(event.target.innerHTML)
     if (event.target.innerHTML === 'Beginer') {
         gameSpeed = 1500;
+        gameTime = 60;
+        gameTimer.textContent = gameTime
     } else if (event.target.innerHTML === 'Medium') {
-        gameSpeed = 100;
+        gameSpeed = 1000;
+        gameTime = 50;
+        gameTimer.textContent = gameTime
     } else if (event.target.innerHTML === 'Expert') {
         gameSpeed = 500;
+        gameTime = 40;
+        gameTimer.textContent = gameTime
     }
     switchToSecondPage()
 
@@ -60,6 +83,7 @@ const switchToSecondPage = () => {
 
 const cleanWindow = () => {
     scoreDisplay.innerHTML = '';
+    score = 0
     onOFF = null;
 }
 
@@ -74,19 +98,17 @@ const moleStart = () => {
         const moleImage = document.createElement("img");
         moleImage.classList.add("mole");
         moleImage.src = './molebody.png'
-
         moleImage.addEventListener('click', () => {
-            console.log("pressed")
+
             score += 5;
             scoreDisplay.innerHTML = score
             moleImage.src = './molebodyAfter.png'
-
             clearTimeout(timer)
-
             setTimeout(() => {
                 hole.removeChild(moleImage)
                 moleStart();
             }, 500)
+
         })
         hole.appendChild(moleImage)
 
@@ -128,20 +150,48 @@ const startGame = () => {
 // }
 
 const gameButtonsAction = (event) => {
+
     if (event.target.innerHTML === 'START') {
         startGame()
+        gameTimer.textContent = gameTime;
+        interval = setInterval(countDownTimer, 1000)
+        countDownTimer();
+
     }
     if (event.target.innerHTML === 'RESET') {
         cleanWindow()
+        clearInterval(interval);
+        interval = '';
+        gameTimer.textContent = gameTime;
+        currentgameTime = null
+
+
     }
     if (event.target.innerHTML === 'GO BACK') {
         welcome.style.display = '';
         gameSection.style.display = 'none';
-        // startButton.style.display = 'unset';
-        // resetButton.style.display = 'unset';
-        // gameButtons.style.display = 'none';
+        clearInterval(interval);
     }
 }
+
+
+const countDownTimer = () => {
+    currentgameTime = gameTimer.textContent
+    currentgameTime--
+    gameTimer.textContent = currentgameTime
+
+    if (currentgameTime == 0) {
+        clearInterval(interval)
+        // cleanWindow()
+        gameOver.style.visibility = 'visible';
+        gameoverScore.innerHTML += score;
+
+    }
+
+}
+
+
+
 
 
 gameButtons.forEach(button => button.addEventListener('click', gameButtonsAction))
